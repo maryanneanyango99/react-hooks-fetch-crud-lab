89c33bb1630e20ea61a5771645a4d47f40e1c8c9
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-07-31 20:46:32 +0300

    files

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,23 +1,29 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question,handleDeleteItem, handleAnsChange }) {
   const { id, prompt, answers, correctIndex } = question;
+ 
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
-
+  function onDelete(){
+    handleDeleteItem(question.id)
+      }
+      function handleChange(e){
+        handleAnsChange(id, parseInt(e.target.value))
+      }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={onDelete}>Delete Question</button>
     </li>
   );
 }
